30bd41358d896a1d0b143e91c955af308eeda7e7
IsaacIngram22 <iingram@students.casciahall.com>
2020-10-28 00:25:59 -0500

    Add documentation and rename method
    Add documentation so that we actually know what each method does. Rename some of the methods so that their names better summarize what they do.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/APIs/MecanumDriveApi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/APIs/MecanumDriveApi.java
@@ -42,7 +42,13 @@ public class MecanumDriveApi {
 
     }
 
-    public void getCartesianValues(double x, double y, double rotation) {
+    /**
+     * Calculates the speed of wheel given the cartesian inputs. These values can be retrieved by using the get[wheel]Speed() methods.
+     * @param x The X value of the left stick (or strafing value)
+     * @param y The Y value of the left stick (or forward value)
+     * @param rotation The X value of the right stick (or rotation value)
+     */
+    public void calculateCartesianValues(double x, double y, double rotation) {
 
         // Reverse the strafe and rotation values
         y = -y;
@@ -56,6 +62,12 @@ public class MecanumDriveApi {
         normalizeWheelSpeeds(wheelSpeeds);
     }
 
+    /**
+     * Calculates the speed of each wheel given the cartesian inputs, and actually drives the robot based on those values via the HardwareMap fed through the constructor.
+     * @param x The X value of the left stick (or strafing value)
+     * @param y The Y value of the left stick (or forward value)
+     * @param rotation The X value of the right stick (or rotation value)
+     */
     public void driveCartesian(double x, double y, double rotation) {
 
         // Reverse the strafe and rotation values
@@ -76,6 +88,10 @@ public class MecanumDriveApi {
 
     }
 
+    /**
+     * Drive the robot forward at the speed specified
+     * @param power
+     */
     public void driveForward(double power) {
         frontLeft.setPower(power);
         frontRight.setPower(power);
@@ -105,18 +121,34 @@ public class MecanumDriveApi {
 
     }
 
+    /**
+     * Get the calculated speed of the front left wheel
+     * @return The speed of the front left wheel
+     */
     public double getFrontLeftSpeed() {
         return wheelSpeeds[0];
     }
 
+    /**
+     * Get the calculated speed of the front right wheel
+     * @return The speed of the front right wheel
+     */
     public double getFrontRightSpeed() {
         return wheelSpeeds[1];
     }
 
+    /**
+     * Get the calculated speed of the rear left wheel
+     * @return The speed of the rear left wheel
+     */
     public double getRearLeftSpeed() {
         return wheelSpeeds[2];
     }
 
+    /**
+     * Get the calculated speed of the rear right wheel
+     * @return The speed of the rear right wheel
+     */
     public double getRearRightSpeed() {
         return wheelSpeeds[3];
     }

0ba58e5ace31a5fb8bd38eca3b19a20da51b410f
IsaacIngram22 <iingram@students.casciahall.com>
2020-11-02 23:49:09 -0600

    Add documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/APIs/MotorControlApi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/APIs/MotorControlApi.java
@@ -12,12 +12,27 @@ public class MotorControlApi {
     double previousRpm;
     double millisToWait = 500;
 
+    private boolean hasRpmBeenInitialized = false;
+
+    /**
+     * Instantiates a new MotorControlApi
+     * @param pGain
+     * @param iGain
+     * @param dGain
+     */
     public MotorControlApi(double pGain, double iGain, double dGain) {
 
         pid = new PidApi(pGain, iGain, dGain);
 
     }
 
+    /**
+     * Instantiates a new MotorControlApi
+     * @param pGain
+     * @param iGain
+     * @param dGain
+     * @param encoderTicksPerMotorRevolution
+     */
     public MotorControlApi(double pGain, double iGain, double dGain, double encoderTicksPerMotorRevolution) {
 
         this.encoderTicksPerMotorRevolution = encoderTicksPerMotorRevolution;
@@ -26,6 +41,14 @@ public class MotorControlApi {
 
     }
 
+    /**
+     * Instantiates a new MotorControlApi
+     * @param pGain
+     * @param iGain
+     * @param dGain
+     * @param encoderTicksPerMotorRevolution
+     * @param millisToWait
+     */
     public MotorControlApi(double pGain, double iGain, double dGain, double encoderTicksPerMotorRevolution, double millisToWait) {
 
         this.millisToWait = millisToWait;
@@ -35,29 +58,49 @@ public class MotorControlApi {
 
     }
 
+    /**
+     * Gets the new motor speed based on a PID formula
+     * @param currentRpm
+     * @param targetRpm
+     * @param currentMotorSpeed
+     * @return
+     */
     public double getNewMotorSpeed(double currentRpm, double targetRpm, double currentMotorSpeed) {
 
         return currentMotorSpeed + pid.getOutput(currentRpm, targetRpm);
 
     }
 
+    /**
+     * Initializes the RPM counter. Note: This must be run before running the getRpm method
+     * @param currentTickCount
+     */
     public void initializeRpmCounter(double currentTickCount) {
 
         previousRpm = 0;
         previousTimeInMillis = System.currentTimeMillis();
         previousTickCount = currentTickCount;
+        hasRpmBeenInitialized = true;
 
     }
 
+    /**
+     * Gets the current RPM of the motor
+     * @param currentTickCount The current position of the motor
+     * @return
+     */
+    public double getRpm(double currentTickCount) {
 
-    public double getRpm(double currentTickCount, Telemetry telemetry) {
+        if(!hasRpmBeenInitialized) {
+            return 0;
+        }
 
         double currentTimeInMillis = System.currentTimeMillis();
 
         if(currentTimeInMillis >= previousTimeInMillis+millisToWait) {
 
-            double previousRotation = previousTickCount/28;
-            double currentRotation = currentTickCount/28;
+            double previousRotation = previousTickCount/encoderTicksPerMotorRevolution;
+            double currentRotation = currentTickCount/encoderTicksPerMotorRevolution;
             double changeInRotation = 0;
 
             if(previousRotation > currentRotation) {
@@ -84,32 +127,59 @@ public class MotorControlApi {
 
     }
 
+    /**
+     * Reset the amount of ticks counted to -0
+     */
     public void resetTicks() {
 
         previousTickCount = 0;
 
     }
 
+    /**
+     * Set the P gain
+     * @param newPGain The new P gain
+     */
     public void setPGain(double newPGain) {
         pid.setPGain(newPGain);
     }
 
+    /**
+     * Set the I gain
+     * @param newIGain The new I gain
+     */
     public void setIGain(double newIGain) {
         pid.setIGain(newIGain);
     }
 
+    /**
+     * Set the D gain
+     * @param newDGain The new D gain
+     */
     public void setDGain(double newDGain) {
         pid.setDGain(newDGain);
     }
 
+    /**
+     * Get the P gain
+     * @return The P gain
+     */
     public double getPGain() {
         return pid.getPGain();
     }
 
+    /**
+     * Get the I gain
+     * @return The I gain
+     */
     public double getIGain() {
         return pid.getIGain();
     }
 
+    /**
+     * Get the D gain
+     * @return The D gain
+     */
     public double getDGain() {
         return pid.getDGain();
     }

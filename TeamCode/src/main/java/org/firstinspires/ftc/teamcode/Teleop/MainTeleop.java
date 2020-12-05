b4ad27ba7194a5a0295d10524fa95818780a847e
IsaacIngram22 <iingram@students.casciahall.com>
2020-12-05 03:17:34 -0600

    Add grabber support and debug mode
    Add controls for the grabber and the claw (although the grabber ones have yet to be tested, since I didn't have the proper cable at my house. Proceeed with caution). Also add the "debug mode" which will enable or disable the wheel speed readout.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MainTeleop.java
@@ -4,29 +4,58 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.APIs.GrabberApi;
 import org.firstinspires.ftc.teamcode.APIs.MecanumDriveApi;
 import org.firstinspires.ftc.teamcode.APIs.TankDriveApi;
 
-@TeleOp(name="Main Teleop")
+@TeleOp(name="Main Teleop V2.4")
 public class MainTeleop extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         MecanumDriveApi drive = new MecanumDriveApi(hardwareMap, 1890, 3);
+        GrabberApi grabber = new GrabberApi(hardwareMap);
+
+        double clawClosedPosition = 0.7;
+        double clawOpenPosition = 0.15;
+
+        boolean debugMode = true;
+        boolean previousA = false;
+
+        grabber.setClawPosition(0.15);
 
         waitForStart();
 
         while(opModeIsActive()) {
 
-            drive.driveCartesian(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+            // Drive based on joystick inputs
+            drive.driveCartesian(0.5*gamepad1.left_stick_x, 0.5*gamepad1.left_stick_y, 0.5*gamepad1.right_stick_x);
 
-            telemetry.addLine(drive.getFrontLeftSpeed() + "(-----)" + drive.getFrontRightSpeed());
-            telemetry.addLine("|       |");
-            telemetry.addLine("|       |");
-            telemetry.addLine("|       |");
-            telemetry.addLine(drive.getRearLeftSpeed() + "(-----)" + drive.getRearRightSpeed());
-            telemetry.addLine("X: " + gamepad1.left_stick_x + " Y: " + gamepad1.left_stick_y + " ROT: " + gamepad1.right_stick_x);
+            // Set the power of the grabber
+            grabber.setGrabberPower(gamepad1.right_trigger-gamepad1.left_trigger);
+
+            // Do some logic for A being pressed and toggling the position of the claw
+            if(gamepad1.a && previousA == false) {
+                if(grabber.getClawPosition() < clawOpenPosition + 0.05 && grabber.getClawPosition() > clawOpenPosition-0.05) {
+                    grabber.setClawPosition(clawClosedPosition);
+                    previousA = true;
+                } else {
+                    grabber.setClawPosition(clawOpenPosition);
+                    previousA = true;
+                }
+            } else if(!gamepad1.a) {
+                previousA = false;
+            }
+
+            if(debugMode) {
+                telemetry.addLine(drive.getFrontLeftSpeed() + "(-----)" + drive.getFrontRightSpeed());
+                telemetry.addLine("|       |");
+                telemetry.addLine("|       |");
+                telemetry.addLine("|       |");
+                telemetry.addLine(drive.getRearLeftSpeed() + "(-----)" + drive.getRearRightSpeed());
+                telemetry.addLine("X: " + gamepad1.left_stick_x + " Y: " + gamepad1.left_stick_y + " ROT: " + gamepad1.right_stick_x);
+            }
             telemetry.update();
         }
 

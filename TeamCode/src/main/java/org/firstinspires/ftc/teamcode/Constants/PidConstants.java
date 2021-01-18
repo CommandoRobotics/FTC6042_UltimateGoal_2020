d83186b8155929ac6bb95cc9c4e5624df8983c44
IsaacIngram22 <iingram@students.casciahall.com>
2021-01-18 15:02:07 -0600

    Update drive forward PID values
    These values work for driving on the carpet in my house, so I went ahead and saved them. Hopefully they'll work on the foam tiles, but we won't know until we test it.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/PidConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/PidConstants.java
@@ -2,8 +2,8 @@ package org.firstinspires.ftc.teamcode.Constants;
 
 public class PidConstants {
 
-    public static final double DRIVE_FORWARD_P = 0.2;
-    public static final double DRIVE_FORWARD_I = 0;
+    public static final double DRIVE_FORWARD_P = 0.15;
+    public static final double DRIVE_FORWARD_I = 1;
     public static final double DRIVE_FORWARD_D = 0;
     public static final double DRIVE_FORWARD_OUTPUT_REDUCTION = 0.01;
 }

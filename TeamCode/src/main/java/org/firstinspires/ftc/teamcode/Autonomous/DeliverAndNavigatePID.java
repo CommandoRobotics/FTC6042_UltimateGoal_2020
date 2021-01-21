26713e64f0606866454f298714cbb68d3fa7acdd
IsaacIngram22 <iingram@students.casciahall.com>
2021-01-21 15:18:13 -0600

    Update new autonomous
    Add to the autonomous so that it moves forward to the launch line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DeliverAndNavigatePID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DeliverAndNavigatePID.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.APIs.ChassisApi;
 import org.firstinspires.ftc.teamcode.Constants.PidConstants;
 
-@Autonomous(name="Deliver to C and Navigate V1.0.1")
+@Autonomous(name="Deliver to C and Navigate V1.1")
 public class DeliverAndNavigatePID extends LinearOpMode {
 
     @Override
@@ -16,11 +16,35 @@ public class DeliverAndNavigatePID extends LinearOpMode {
 
         waitForStart();
 
-        chassis.startDriveForwardPid(-110, PidConstants.DRIVE_FORWARD_P, PidConstants.DRIVE_FORWARD_I, PidConstants.DRIVE_FORWARD_D);
-
+        // Drive to target zone C
+        telemetry.addLine("Status: Reversing to target C");
+        telemetry.addLine("PID is ENABLED");
+        telemetry.update();
+        chassis.startDriveForwardPid(-110);
         while(chassis.isActionRunning() && opModeIsActive()) {
             chassis.updatePositionDriveForward();
         }
+
+        // Wait 2 seconds
+        long waitStartTimeInMillis = System.currentTimeMillis();
+        while(System.currentTimeMillis() < waitStartTimeInMillis+2000 && opModeIsActive()) {
+            telemetry.addLine("Status: Waiting");
+            long timeRemainingInMillis = (waitStartTimeInMillis+2000)-System.currentTimeMillis();
+            telemetry.addLine("Time remaining: " + timeRemainingInMillis + " millis");
+            telemetry.update();
+        }
+
+        // Reverse to launch line
+        telemetry.addLine("Status: Driving to launch line");
+        telemetry.addLine("PID is ENABLED");
+        telemetry.update();
+        chassis.startDriveForwardPid(24);
+        while(chassis.isActionRunning() && opModeIsActive()) {
+            chassis.updatePositionDriveForward();
+        }
+
+        telemetry.addLine("Status: Program finished");
+        telemetry.update();
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/PidConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/PidConstants.java
@@ -6,4 +6,6 @@ public class PidConstants {
     public static final double DRIVE_FORWARD_I = 1;
     public static final double DRIVE_FORWARD_D = 0.1;
     public static final double DRIVE_FORWARD_OUTPUT_REDUCTION = 0.01;
+    public static final double DRIVE_FORWARD_DEAD_ZONE = 5;
+
 }

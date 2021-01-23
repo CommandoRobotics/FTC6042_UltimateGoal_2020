79aee09baeac8c80647a04f777036f7103bcfa1f
IsaacIngram22 <iingram@students.casciahall.com>
2021-01-23 09:49:55 -0600

    Update inches
    Update the inches to drive because we were a little short before

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DeliverAndNavigatePID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DeliverAndNavigatePID.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.APIs.ChassisApi;
 import org.firstinspires.ftc.teamcode.Constants.PidConstants;
 
-@Autonomous(name="Deliver to C and Navigate V1.1")
+@Autonomous(name="Deliver to C and Navigate V1.2")
 public class DeliverAndNavigatePID extends LinearOpMode {
 
     @Override
@@ -38,7 +38,7 @@ public class DeliverAndNavigatePID extends LinearOpMode {
         telemetry.addLine("Status: Driving to launch line");
         telemetry.addLine("PID is ENABLED");
         telemetry.update();
-        chassis.startDriveForwardPid(24);
+        chassis.startDriveForwardPid(39);
         while(chassis.isActionRunning() && opModeIsActive()) {
             chassis.updatePositionDriveForward();
         }
